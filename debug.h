ff244bf4f84dcc188010c6fba79737ca43f4bb96
Yusuf Reza Hasnat <118974654+hasnat0006@users.noreply.github.com>
2023-07-18 23:39:02 +0600

    Add files via upload

--- debug.h
+++ debug.h
@@ -3,7 +3,7 @@
 #define int long long
 #define float long double
 #define pb push_back
-#define cerr clog
+// #define cerr clog
 
 using namespace std;
 
@@ -23,7 +23,7 @@ template<typename T, typename V>
 void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
 template<typename T>
 void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
+void _print() {cerr << "";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
@@ -44,12 +44,14 @@ template <class T> void _print(set <T> v);
 template <class T, class V> void _print(map <T, V> v);
 template <class T> void _print(multiset <T> v);
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << " ]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << " ]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << " ]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << " ]";}
 
 
-#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
-#define dbgc(x...) cerr << "\033[1;31m[" << #x << "]\033[1;31m \033[1;33m = \033[0m \033[0m \033[1;32m[" ; _print(x); cerr<<"\033[0m\n";
+    #define dbg(x...) cerr << "[ " << #x << " ] = [ "; _print(x)  ; cerr << " ]" << endl;
+// #define dbg(x...) cerr << "[ " << #x << " ] = [ "; _print(x);
+
+    #define dbgc(x...) cerr << "\033[1;31m[ " << #x << " ]\033[1;31m \033[1;33m = \033[0m \033[0m \033[1;32m[ " ; _print(x); cerr<<" ]\033[0m"; cerr << endl;
 
