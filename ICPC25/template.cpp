b09c84c60650ca6816c411a1340bd86e8e19b225
hasnat0006 <yusufrezahasnat0006@gmail.com>
2025-11-12 16:55:53 +0600

    add ICPC 25 setup

---
+++ ICPC25/template.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define vf(v) (v).begin(), (v).end()
+#define vr(v) (v).rbegin(), (v).rend()
+
+const ll mod = 1e9 + 7;
+const ll inf = 1e18;
+
+void solve() {
+    cout << "ICPC 25!\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 1; i <= t; i++) {
+        // cout << "Case " << i << ": ";
+        solve(); 
+    }
+    return 0;
+}
