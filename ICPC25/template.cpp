86605838946f48c9d0f1e379c4313c30df166878
hasnat0006 <yusufrezahasnat0006@gmail.com>
2025-11-14 00:29:58 +0600

    Refactor code structure for improved readability and maintainability

--- ICPC25/template.cpp
+++ ICPC25/template.cpp
@@ -1,11 +1,7 @@
 #pragma GCC optimize("O3")
 #include<bits/stdc++.h>
-
 using namespace std;
-
 #define ll long long
-#define vf(v) (v).begin(), (v).end()
-#define vr(v) (v).rbegin(), (v).rend()
 
 const ll mod = 1e9 + 7;
 const ll inf = 1e18;
